06a6455ad03a08327c8cf8e45e25bd154fe5345e
Henri Bourcereau <henri.bourcereau@gmail.com>
2017-06-24 10:46:39 +0200

    UI updates

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { StyleSheet, Text, View, ScrollView, Easing, Animated, Image } from 'react-native';
+import { StyleSheet, Dimensions, Text, View, ScrollView, Easing, Animated, Image } from 'react-native';
 
 import { Font } from 'expo';
 
@@ -9,6 +9,7 @@ import { Card, Icon, Button } from 'react-native-elements';
 import {randomQuoteId, randomQuote} from './patamancy';
 
 export default class App extends React.Component {
+
   state = {
     spinValue: new Animated.Value(0),
     fontLoaded: false,
@@ -33,7 +34,7 @@ export default class App extends React.Component {
         this.state.spinValue,
         {
           toValue: 1,
-          duration: 400,
+          duration: 700,
           easing: Easing.linear
         }
       ), 
@@ -49,7 +50,8 @@ export default class App extends React.Component {
   }
 
   updateQuote = () => {
-    this.setState({showCard: false, showIntroduction: false})
+    // this.setState({showCard: false, showIntroduction: false})
+    this.setState({ showIntroduction: false, showCard: false})
     this.animateSpin(() => {
         this.setState({showCard: true})
       })
@@ -62,26 +64,36 @@ export default class App extends React.Component {
         inputRange: [0, 1],
         outputRange: ['360deg', '0deg']
       })
+    let opacityVal = this.state.spinValue.interpolate({
+        inputRange: [0, 0.4, 0.8, 1],
+        outputRange: [0, 1, 1, 0]
+      })
 
     return (
       <View style={styles.container}>
       <ScrollView>
-      <View style={{ alignItems: 'center', justifyContent: 'center'}}>
-          <Icon iconStyle={styles.button} reverse raised type="material-community" name="dice-3" size={42} color="gray"  onPress={this.updateQuote} />
+      <View style={{ alignItems: 'center', justifyContent: 'center', margin: 20 }}>
+          <Button title="Explorer le clinamen" raised size={42} onPress={this.updateQuote} />
+          {/*<Icon iconStyle={styles.button} raised type="material-community" name="dice-3" size={42} color="gray"  onPress={this.updateQuote} /> */} 
       </View>
       {
         this.state.fontLoaded ? (
           this.state.showIntroduction ?  (<Introduction />) : 
-          this.state.showCard  ? ( <QuoteCard quote={this.state.quote} />) : (
-            <Animated.View style={{transform: [{rotate: spin}]}} >
-            <View style={{padding:20}}>
-              <Image style={{ width:300, height:300}} source={require('./assets/black-spiral-lollipop-600.png')} />
-              </View>
-            </Animated.View>
-          ) 
+          this.state.showCard  ? ( <QuoteCard quote={this.state.quote}/>) : null 
         ) : null
       }
       </ScrollView>
+
+      {
+        this.state.showCard ? null : (
+          <Animated.View style={[styles.overlay, {top: 130, opacity: 0.8, transform: [{rotate: spin}]}]} >
+            <View style={{padding:20}}>
+              <Animated.Image style={{ width:300, height:300, opacity:opacityVal}} source={require('./assets/black-spiral-lollipop-600.png')} />
+            </View>
+          </Animated.View>
+        )
+      }
+
       </View>
     );
   }
@@ -91,8 +103,10 @@ class Introduction extends React.Component {
   render(){
     return (
         <View style={styles.introduction}>
-          <View style={{ paddingTop: 50 }}>
-            <Text style={{ fontSize: 26, fontFamily: 'lora-regular'}}>« Tout est dans Faustroll »</Text>
+          <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+            <Text style={{ width: '90%', fontSize: 26, fontFamily: 'lora-regular'}}>« Tout est dans Faustroll »</Text>
+          </View>
+          <View style={{ width: '90%', flexDirection: 'row', justifyContent: 'flex-end' }}>
             <Text style={{ fontSize: 20, fontFamily: 'lora-italic'}}>Boris Vian</Text>
           </View>
         </View>
@@ -103,6 +117,7 @@ class Introduction extends React.Component {
 class QuoteCard extends React.Component {
     render() {
       return (
+        <Animated.View>
       <Card style={styles.card}>
         <View style={{ flexDirection: 'column', flex: 1, justifyContent: 'center', padding: 20 }}>
           <Text style={{ fontFamily: 'lora-regular', fontSize: 18 }}> {this.props.quote.quote}</Text>
@@ -113,25 +128,37 @@ class QuoteCard extends React.Component {
           <Text style={styles.cardInfos}>[ {this.props.quote.position/1000}% ]</Text>
         </View>
       </Card>
+        </Animated.View>
     )
    }
  }
+
+var { width, height } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: 30,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  // Flex to fill, position absolute, 
+  // Fixed left/top, and the width set to the window width
+  overlay: {
+    flex: 1,
+    position: 'absolute',
+    width: width,
+    left: 0,
+    top: 0,
+  },  
   button: {
     elevation: 5,
   },
   introduction: {
+    height: 400,
     flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#fff',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
   },
